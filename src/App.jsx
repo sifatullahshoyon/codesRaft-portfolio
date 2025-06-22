5c620f26c4ec91aa5500208492cbb5531ec85c97
freelancersifat627e <freelancersifat627@gmail.comm>
2025-06-22 16:05:13 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { SparklesText } from "@/components/magicui/sparkles-text";
+
+function App() {
+  return (
+    <div className="container mx-auto ">
+      {" "}
+      <SparklesText>Sparkles Text</SparklesText>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
