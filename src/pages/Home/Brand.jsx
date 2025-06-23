5299d7cce702261e3868b65d2ec36bdbc7c5ddff
freelancersifat627e <freelancersifat627@gmail.comm>
2025-06-23 14:51:48 +0600

    brand section done

---
+++ src/pages/Home/Brand.jsx
@@ -0,0 +1,37 @@
+import Marquee from "react-fast-marquee";
+import img1 from "../../assets/images/brands/logo1.png";
+import img2 from "../../assets/images/brands/logo2.jpg";
+import img3 from "../../assets/images/brands/logo3.jpg";
+import img4 from "../../assets/images/brands/logo4.jpg";
+import img5 from "../../assets/images/brands/logo5.jpg";
+import img6 from "../../assets/images/brands/logo6.jpg";
+import img7 from "../../assets/images/brands/logo7.jpg";
+import Container from "../../components/shared/Container";
+
+const Brand = () => {
+  return (
+    <section className="mt-16">
+      {" "}
+      <Container>
+        <Marquee>
+          <div className="flex flex-row justify-between items-center gap-6">
+            <img src={img1} alt="img 1" />
+            <img src={img2} alt="img 1" />
+            <img src={img3} alt="img 1" />
+            <img src={img4} alt="img 1" />
+            <img src={img5} alt="img 1" />
+          </div>
+          <div className="flex flex-row justify-between items-center gap-6">
+            <img src={img1} alt="img 1" />
+            <img src={img2} alt="img 1" />
+            <img src={img3} alt="img 1" />
+            <img src={img4} alt="img 1" />
+            <img src={img5} alt="img 1" />
+          </div>
+        </Marquee>
+      </Container>
+    </section>
+  );
+};
+
+export default Brand;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import AboutMe from "./AboutMe";
+import Brand from "./Brand";
 import Hero from "./Hero";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <Hero />
       <AboutMe />
+      <Brand />
     </>
   );
 };
